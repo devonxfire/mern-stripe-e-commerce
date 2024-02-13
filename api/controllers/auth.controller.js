25359dabe18a64854dce6f840ac96d2ac42b17da
devonmartindale <devonmartindale@gmail.com>
2024-02-13 15:09:32 +0200

    complete signin and signup

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,10 +1,63 @@
 import { errorHandler } from "../utils/errorHandler.js";
+import { User } from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
 
 // Signup
-export const signup = (req, res, next) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
 
+  // Required fields check
   if (!username || !email || !password) {
     return next(errorHandler(401, "All fields are required"));
   }
+
+  try {
+    // Check if user already exists
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      return next(errorHandler(401, "User already exists"));
+    }
+
+    // Hash password
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+
+    // Create new user
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    // Save user to DB
+    await newUser.save();
+    return res.status(201).json("User created successfully");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // Required fields check
+  if (!email || !password) {
+    return next(errorHandler(401, "All fields are required"));
+  }
+
+  // Check if user exists
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return next(errorHandler(404, "User does not exist"));
+  }
+
+  // Compare passwords
+  const isValidPassword = bcryptjs.compareSync(password, user.password);
+
+  if (!isValidPassword) {
+    return next(errorHandler(403, "Invalid password"));
+  }
+
+  return res.status(200).json("User signed in successfully");
 };
