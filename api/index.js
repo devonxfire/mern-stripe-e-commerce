850f4fd6d75fe487fa878db0fd80abe54b60dc1b
devonmartindale <devonmartindale@gmail.com>
2024-02-13 14:09:57 +0200

    initial commit

---
+++ api/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+// Connect to DB
+
+// Middleware
+
+// Error handling
+
+// Routes
+
+const app = express();
+
+app.listen(3000, () => console.log("Listening on port 3000"));
